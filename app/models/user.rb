c8c75e670025caefc0eff32a46c8bfb63ff8178f
francisco15 <franciscohuertas38@gmail.com>
2017-02-17 10:23:09 -0500

    creadas las validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+  validates :email, uniqueness: true, format: /@/
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 8..20 }
+  validates :password, confirmation: true
 end
