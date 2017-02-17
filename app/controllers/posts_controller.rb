e30077ca3addf0fc61d9544d741b310e9798f77b
francisco15 <franciscohuertas38@gmail.com>
2017-02-17 09:51:25 -0500

    agregada vista de sesion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :private_access, except: [:index, :show]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
 end
