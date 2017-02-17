91228edf3ce5cbb0fdb2cbb7e3b04149f1717962
francisco15 <franciscohuertas38@gmail.com>
2017-02-17 13:28:40 -0500

    arreglado error de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+  has_secure_password
   validates :email, uniqueness: true, format: /@/
   validates :password, presence: true, on: :create
   validates :password, length: { in: 8..20 }
